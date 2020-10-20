f62aa4a260f39f51c769dd2fee2f7d0f8593287b
MinSeok <skymin623@gmail.com>
2020-10-20 21:06:15 +0900

    Updates

---
+++ precache-manifest.4bb71efa7f7f74e341a1c2662f3057e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45707f5c10bd40cf2af581fb858adbee",
+    "url": "/moview_app_2019/index.html"
+  },
+  {
+    "revision": "5827fdd42331c2be178b",
+    "url": "/moview_app_2019/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "4d7c806d0e258b226914",
+    "url": "/moview_app_2019/static/js/2.00a09d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moview_app_2019/static/js/2.00a09d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5827fdd42331c2be178b",
+    "url": "/moview_app_2019/static/js/main.db5ae77b.chunk.js"
+  },
+  {
+    "revision": "ca32ba58de8ec83763ff",
+    "url": "/moview_app_2019/static/js/runtime-main.315d9b78.js"
+  }
+]);
